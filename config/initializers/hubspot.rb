3e92e73dae33cf7052e83cea10e54c0f760d3380
alegrand42 <alegrand@student.42.fr>
2019-07-23 13:54:52 +0200

    fix: config syntax

--- config/initializers/hubspot.rb
+++ config/initializers/hubspot.rb
@@ -5,9 +5,9 @@ Hubspot.configure({
   base_url: "https://api.hubapi.com",
   portal_id: ENV['PORTAL_ID'],
   logger: Logger.new(nil),
-  access_token: ['ACCESS_TOKEN']
+  access_token: ['ACCESS_TOKEN'],
   client_id: ENV['CLIENT_ID'],
   client_secret: ENV['CLIENT_SECRET'],
-  read_timeout: nil, # or :timeout to set read_timeout and open_timeout
+  read_timeout: nil,
   open_timeout: nil,
 })
